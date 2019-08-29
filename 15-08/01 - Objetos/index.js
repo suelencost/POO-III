08861dc994cb64f3e220af5043ec0e8d3f926c83
unknown <suelencosta2608@gmail.com>
2019-08-29 20:26:25 -0300

    init

---
+++ 15-08/01 - Objetos/index.js
@@ -0,0 +1,17 @@
+function verificaobjeto (armazenamento, velocidade, marca,valor){
+    var pendrive = new Object();
+    pendrive.armazenamento = armazenamento;
+    pendrive.velocidade = velocidade;
+    pendrive.marca = marca;
+    pendrive.valor = valor;
+    console.log(pendrive);
+    
+}
+
+
+function Chimarrao(eravamate, bomba, cuia, agua) {
+        this.eravamate = eravamate;
+        this.bomba = bomba;
+        this.cuia = cuia;
+        this.agua = agua;
+    }
